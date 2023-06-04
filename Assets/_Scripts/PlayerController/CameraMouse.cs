a4316accfc983aa3387e1cbfad9c293c7d8395c2
quyet <85507118+lequyet2001@users.noreply.github.com>
2023-06-04 21:32:08 +0700

    Update CameraMouse.cs

--- Assets/_Scripts/PlayerController/CameraMouse.cs
+++ Assets/_Scripts/PlayerController/CameraMouse.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMouse : MonoBehaviour {
-
 	Vector2 mouseLook;
 	Vector2 smoothV;
 	public float sensitivity = 5.0f;
@@ -19,7 +18,6 @@ public class CameraMouse : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		var md = new Vector2 (Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-
 		md = Vector2.Scale (md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
 		smoothV.x = Mathf.Lerp (smoothV.x, md.x, 1f/smoothing);
 		smoothV.y = Mathf.Lerp (smoothV.y, md.y, 1f/smoothing);
