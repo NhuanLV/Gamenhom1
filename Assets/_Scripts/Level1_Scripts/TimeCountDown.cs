f9aa2b2f6b8363bb5872820bde7e5a2ad1746f2d
quyet <85507118+lequyet2001@users.noreply.github.com>
2023-06-04 21:33:16 +0700

    Update TimeCountDown.cs

--- Assets/_Scripts/Level1_Scripts/TimeCountDown.cs
+++ Assets/_Scripts/Level1_Scripts/TimeCountDown.cs
@@ -19,7 +19,7 @@ public class TimeCountDown : MonoBehaviour {
 	void Update () {
 		float t = Time.time - startTime;
 		string minutes = ((int)t / 60).ToString ();
-		string seconds = (t % 60).ToString ("f0");
+		string seconds = (t %  60).ToString ("f0 ");
 		timeText.text = "Time: " + minutes + " : " + seconds;
 		//if (minutes.Contains ("0") && seconds.Contains ("0")) {
 		//	pauseButton.SetActive (false);
