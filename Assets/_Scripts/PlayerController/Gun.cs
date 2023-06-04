16df7cc922f371f167bcc43cb2c84690c137171d
quyet <85507118+lequyet2001@users.noreply.github.com>
2023-06-04 21:37:24 +0700

    Update Gun.cs

--- Assets/_Scripts/PlayerController/Gun.cs
+++ Assets/_Scripts/PlayerController/Gun.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour {
-
 	public float damage = 10f;
 	public float range = 100f;
-
 	public Camera fpsCam;
 	
 	// Update is called once per frame
